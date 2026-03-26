aa28c642c42a65595d6ca56998e75ad5c0b0d91f
pastsawutkhon <pastsawut.khon@bumail.net>
2026-03-26 23:30:08 +0700

    fix movement horizontal axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,16 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
+        float moveX = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
 
         Vector3 velocity = rb.linearVelocity;
 
@@ -28,5 +37,9 @@ public class Player : MonoBehaviour
         Vector3 pos = rb.position;
         pos.x = Mathf.Clamp(pos.x, -10f, 10f);
         rb.MovePosition(pos);
+
+        
     }
+
+
 }
